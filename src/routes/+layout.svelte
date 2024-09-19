693b696356bd5a9773dd81d6f132c7e7391ecd0d
j1gc <142526884+j1gc@users.noreply.github.com>
2024-09-19 12:23:29 +0000

    added assets from tutorial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css';
+</script>
+
+<main>
+	<nav>
+		<a href="./">Startseite</a>
+		<a href="./preise">Unsere Preise</a>
+		<a href="./inhalt">Bilder von unseren Produkten</a>
+		<a href="./inhalt">Kontakt und Impressum</a>
+	</nav>
+	<slot />
+</main>
+
